0e846a7852df76a27ada782606d2c29c191147a2
thiagoarce <thiagoarce@outlook.com>
2020-01-26 17:42:29 -0400

    rotas encapsuladas

--- server.js
+++ server.js
@@ -1,30 +1,7 @@
-const app = require('./src/config/custon-express')
+const app = require('./src/config/custom-express')
 
 app.listen(3000, function () {
     console.log(`Servidor rodando na porta 3000`)
 });
 
-app.get('/', function (req, resp) {
-    resp.send(`
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1> Casa do Código </h1>
-                    </body> 
-                </html>
-            `);
-});
-app.get('/livros', function (req, resp) {
-    resp.send(`
-                            <html>
-                                <head>
-                                    <meta charset="utf-8">
-                                </head>
-                                <body>
-                                    <h1> Listagem de livros </h1>
-                                </body> 
-                            </html>
-                        `);
-});
+
