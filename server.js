e360c4c09fb69bbeeba992a52cd1cf0d4247b012
thiagoarce <thiagoarce@outlook.com>
2020-01-26 17:36:50 -0400

    express encapsulado

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
-const http = require('http');
+const app = require('./src/config/custon-express')
 
-const servidor = http.createServer(function (req, resp) {
-   
-    let html = '';
-    if (req.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-        `;
-    }else if(req.url == '/livros'){
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Listagem de livros </h1>
-            </body> 
-        </html>
-    `;
-    }
-
-    // outros else-if
-
-    resp.end(html);
+app.listen(3000, function () {
+    console.log(`Servidor rodando na porta 3000`)
 });
-servidor.listen(3000);
+
+app.get('/', function (req, resp) {
+    resp.send(`
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Casa do Código </h1>
+                    </body> 
+                </html>
+            `);
+});
+app.get('/livros', function (req, resp) {
+    resp.send(`
+                            <html>
+                                <head>
+                                    <meta charset="utf-8">
+                                </head>
+                                <body>
+                                    <h1> Listagem de livros </h1>
+                                </body> 
+                            </html>
+                        `);
+});
