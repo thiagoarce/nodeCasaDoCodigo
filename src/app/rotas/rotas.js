2d0662671c4f767cdccb4a1764eb86666c015d0f
thiagoarce <thiagoarce@outlook.com>
2020-01-26 18:23:10 -0400

    Listagem estática de livros

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -11,17 +11,11 @@ module.exports = app => {
                     </html>
                 `);
     });
-    app.get('/livros', function (req, resp) {
-        resp.send(`
-                                <html>
-                                    <head>
-                                        <meta charset="utf-8">
-                                    </head>
-                                    <body>
-                                        <h1> Listagem de livros </h1>
-                                    </body> 
-                                </html>
-                            `);
+    app.get('/livros', function(req, resp) {
+        resp.marko(
+            require('../views/livros/lista/lista.marko')
+    
+        );
     });
 
 }
