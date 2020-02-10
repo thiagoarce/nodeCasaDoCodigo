275e40b0c9662a2e86f37b988491b38ea5d29ec1
thiagoarce <thiagoarce@outlook.com>
2020-02-10 16:51:17 -0400

    implementado padrão DAO

---
+++ src/app/infra/livro-dao.js
@@ -0,0 +1,15 @@
+class LivroDao {
+
+    constructor(db) {
+        this._db = db;
+    }
+
+    lista(callback) {
+        this._db.all(
+            'SELECT * FROM livros',
+            (erro, resultados) =>
+                callback(erro, resultados)
+        )
+    }
+}
+module.exports = LivroDao;
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,4 +1,5 @@
-const db = require('../../config/database')
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
 
 module.exports = app => {
     app.get('/', function (req, resp) {
@@ -14,7 +15,8 @@ module.exports = app => {
                 `);
     });
     app.get('/livros', function(req, resp) {
-        db.all('SELECT * FROM livros', function(erro, resultados){
+        const livroDao = new LivroDao(db);
+        livroDao.lista(function(erro, resultados){
             resp.marko(
                 require('../views/livros/lista/lista.marko'),
                 {
@@ -22,9 +24,7 @@ module.exports = app => {
                 }
         
             );
-        });
-
-
+        })
     });
 
 }
